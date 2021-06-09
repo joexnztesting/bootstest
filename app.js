51a22731625eb39625b10dfd276e91a23abfe6b4
joexnztesting <joe.reposgithub@gmail.com>
2021-06-09 17:19:30 -0300

    21646

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const $tituloProd = document.querySelector('.titu-prod'),
 $firstCard = document.querySelectorAll('.card:first-child'),
 $lastCard = document.querySelectorAll('.card:last-child'),
 $sectionAcerca = document.querySelectorAll('.acerca'),
-$tituloEnvios = document.querySelector('.titu-envios'),
-$tituloSocial = document.querySelectorAll('.titu-social');;
+$tituloAcerca = document.querySelector('.titu-acerca'),
+$tituloContacto = document.querySelector('.titu-social'),
+$formu = document.querySelectorAll('.contact-form');
 
 
 
@@ -22,23 +23,24 @@ const tituOut1 = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
     $tituloProd.classList.add('titu-opac-out');
-    $tituloEnvios.classList.add('titu-sticky');
+    $tituloAcerca.classList.add('titu-sticky');
   }
 })
 }  
 const tituOut2 = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
-    $tituloEnvios.classList.remove('titu-sticky');
+    $tituloAcerca.classList.remove('titu-sticky');
+    $tituloContacto.classList.add('titu-sticky');
   }
 })
 }
 
 const observer1 = new IntersectionObserver(tituIn, {threshold:0.2});
-const observer2 = new IntersectionObserver(tituOut1, {threshold:0.75});
+const observer2 = new IntersectionObserver(tituOut1, {threshold:0.6});
 const observer3 = new IntersectionObserver(tituOut2, {threshold:0.2});
 
 $firstCard.forEach((el)=>observer1.observe(el));
 $lastCard.forEach((el)=>observer1.observe(el));
 $sectionAcerca.forEach((el)=>observer2.observe(el));
-$tituloSocial.forEach((el)=>observer3.observe(el));
+$formu.forEach((el)=>observer3.observe(el));

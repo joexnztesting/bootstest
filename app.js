60ff1cfae31fc7ad81d15e43df32b19dcf9f509e
joexnztesting <joe.reposgithub@gmail.com>
2021-06-13 01:06:09 -0300

    365435

--- app.js
+++ app.js
@@ -1,54 +1,75 @@
 
 
 
-const $tituloProd = document.querySelector('.titu-prod'),
-$firstCard = document.querySelectorAll('.card:first-child'),
+const $tituProd = document.querySelector('.titu-prod'),
+$tituAcerca = document.querySelector('.titu-acerca'),
+$tituContacto = document.querySelector('.titu-social'),
+//$firstCard = document.querySelectorAll('.card:first-child'),
 $lastCard = document.querySelectorAll('.card:last-child'),
 $sectionAcerca = document.querySelectorAll('.acerca'),
-$tituloAcerca = document.querySelector('.titu-acerca'),
-$tituloContacto = document.querySelector('.titu-social'),
-$formu = document.querySelectorAll('.contact-form');
-//$socialIcons = document.querySelector('.row-icons'),
-//$sumate = document.querySelector('.contacto-sumate');
+$fondoManos = document.querySelectorAll('.fondo-manos'),
+$socialIcons = document.querySelector('.row-icons'),
+$formuinput = document.querySelector('.formu-input'),
+$formu = document.querySelectorAll('.formu'),
+$formuAb = document.querySelector('.formu'),
+$footer = document.querySelectorAll('.footer'),
+$sumate = document.querySelector('.contacto-sumate');
 
 
 
 const tituIn = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
-    $tituloProd.classList.remove('titu-opac-out');
-    $tituloProd.classList.add('titu-header-in');
+    //$tituProd.classList.remove('titu-opac-out');
+    //$tituProd.classList.add('titu-header-in');
   }
 })
 }
 const tituOut1 = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
-    $tituloProd.classList.add('titu-opac-out');
-    $tituloAcerca.classList.add('titu-sticky');
+    //$tituProd.classList.add('titu-opac-out');
   }
 })
-}  
+} 
+
 const tituOut2 = (entries)=>{
-entries.forEach(entry=>{
-  if(entry.isIntersecting){
-    $tituloAcerca.classList.remove('titu-sticky');
-    $tituloContacto.classList.add('titu-sticky');
-    //$socialIcons.classList.add('titu-sticky');
-    //$sumate.classList.add('titu-sticky');
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      $socialIcons.classList.add('visi-out');
+      $sumate.classList.add('visi-out'); 
+    }
+    if(!entry.isIntersecting){
+      $socialIcons.classList.remove('visi-out');
+      $sumate.classList.remove('visi-out'); 
+    }
+  })
+}
+const tituIn2 = (entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      $socialIcons.classList.add('visi-out');
+      $sumate.classList.add('visi-out'); 
   }
 })
 }
 
 const observer1 = new IntersectionObserver(tituIn, {threshold:0.2});
-const observer2 = new IntersectionObserver(tituOut1, {threshold:0.6});
-const observer3 = new IntersectionObserver(tituOut2, {threshold:0.2});
+const observer2 = new IntersectionObserver(tituOut1, {threshold:0.9});
+const observer3 = new IntersectionObserver(tituOut2, {threshold:0.6});
+const observer4 = new IntersectionObserver(tituIn2, {threshold:0.5});
 
-$firstCard.forEach((el)=>observer1.observe(el));
+//$firstCard.forEach((el)=>observer1.observe(el));
 $lastCard.forEach((el)=>observer1.observe(el));
 $sectionAcerca.forEach((el)=>observer2.observe(el));
 $formu.forEach((el)=>observer3.observe(el));
+$footer.forEach((el)=>observer4.observe(el));
 
-window.addEventListener('DOMContentLoaded',(e)=>{
-  $tituloProd.classList.add('titu-header-in');
+
+window.addEventListener('click',(e)=>{
+  if(e.target.matches('.formu-input')){
+    alert('hi') ;
+    //$formuAb.classList.add('form-abslt');
+  }
+  //$tituloProd.classList.add('titu-sticky');
 })

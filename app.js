412e469449b4d6b931b74da085db274790e14c6b
joexnztesting <joe.reposgithub@gmail.com>
2021-07-05 01:48:51 -0300

    7845343

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const $span1 = document.querySelectorAll('.span-1');
+const $span2 = document.querySelectorAll('.span-2');
+const $tituHuerta = document.querySelector('.titu-huerta');
 
 const $socialIcons = document.querySelectorAll('.row-icons');
 const $marcoPlantas = document.querySelectorAll('.marco-plantas');
@@ -14,6 +17,16 @@ entries.forEach(entry=>{
   }
 })
 }
+const stickyOut2 = (entries)=>{
+entries.forEach(entry=>{
+  if(entry.isIntersecting){
+    if($tituHuerta.classList.contains('separador')){
+      $tituHuerta.classList.add('ocultar-titu')
+      $tituHuerta.classList.remove('mostrar-titu')
+    }    
+  }
+})
+}
 const stickyIn = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
@@ -24,13 +37,26 @@ entries.forEach(entry=>{
   }
 })
 }
+const stickyIn2 = (entries)=>{
+entries.forEach(entry=>{
+  if(entry.isIntersecting){
+    if($tituHuerta.classList.contains('ocultar-titu')){
+      $tituHuerta.classList.add('mostrar-titu')
+      $tituHuerta.classList.remove('ocultar-titu')
+    }    
+  }
+})
+}
 
 const observer1 = new IntersectionObserver(stickyOut, {threshold:0.4});
 const observer2 = new IntersectionObserver(stickyIn, {threshold:0.3});
+const observer3 = new IntersectionObserver(stickyOut2, {threshold:0.3});
+const observer4 = new IntersectionObserver(stickyIn2, {threshold:0.1});
 
 $socialIcons.forEach((el)=>observer1.observe(el));
 $marcoPlantas.forEach((el)=>observer2.observe(el));
-
+$span2.forEach((el)=>observer3.observe(el));
+$span1.forEach((el)=>observer4.observe(el));
 
 
 
--- slider.js
+++ slider.js
@@ -11,6 +11,7 @@ window.addEventListener('DOMContentLoaded',(e)=>{
   const $slide5 = document.querySelector('.slide:nth-child(5)');
 
   const leftTocenter = function(e){
+    e.classList.add('centrado')
     e.classList.add('opacity-in') 
     e.classList.add('left-to-center') 
     e.classList.remove('center-to-left') 
@@ -18,6 +19,7 @@ window.addEventListener('DOMContentLoaded',(e)=>{
     e.classList.remove('center-to-right')
   }
   const centerToleft = function(e){
+    e.classList.remove('centrado')
     e.classList.add('opacity-in')
     e.classList.add('center-to-left') 
     e.classList.remove('left-to-center')    
@@ -25,6 +27,7 @@ window.addEventListener('DOMContentLoaded',(e)=>{
     e.classList.remove('center-to-right')    
   }
   const rightTocenter = function(e){
+    e.classList.add('centrado')
     e.classList.add('opacity-in') 
     e.classList.add('right-to-center') 
     e.classList.remove('center-to-right')
@@ -32,6 +35,7 @@ window.addEventListener('DOMContentLoaded',(e)=>{
     e.classList.remove('center-to-left')
   }
   const centerToright = function(e){
+    e.classList.remove('centrado')
     e.classList.add('opacity-in')
     e.classList.add('center-to-right') 
     e.classList.remove('right-to-center')
@@ -42,69 +46,55 @@ window.addEventListener('DOMContentLoaded',(e)=>{
   const c1fl = function(e){  
     leftTocenter($slide1)  
     centerToright($slide2)
-    $slide1.classList.add('centrado')
-    $slide2.classList.remove('centrado')
+    $selectRight.classList.remove('r0')
+  }
+  const c2fl = function(e){
+    leftTocenter($slide2)
+    centerToright($slide3)
+    $selectRight.classList.add('fk')   
+  }
+  const c3fl = function(e){
+    leftTocenter($slide3)     
+    centerToright($slide4)
+  }
+  const c4fl = function(e){
+  leftTocenter($slide4)      
+  centerToright($slide5)  
   }
   const c5fl = function(e){
     leftTocenter($slide5)
     centerToright($slide1)
-    $slide5.classList.add('centrado')
-    $slide1.classList.remove('centrado')
-  }
-  const c4fl = function(e){
-    leftTocenter($slide4)      
-    centerToright($slide5)  
-    $slide4.classList.add('centrado')
-    $slide5.classList.remove('centrado')  
-  }
-  const c3fl = function(e){
-    leftTocenter($slide3)     
-    centerToright($slide4)   
-    $slide3.classList.add('centrado')
-    $slide4.classList.remove('centrado')  
-  }
-  const c2fl = function(e){
-    leftTocenter($slide2)
-    centerToright($slide3) 
-    $slide2.classList.add('centrado')
-    $slide3.classList.remove('centrado')    
+    $selectLeft.classList.add('l0')
   }
   
   const c1fr = function(e){
     rightTocenter($slide1)
     centerToleft($slide5)
-    $slide1.classList.add('centrado')
-    $slide5.classList.remove('centrado')
+    $selectLeft.classList.remove('l0')
   }
   const c2fr = function(e){
     rightTocenter($slide2)
     centerToleft($slide1)
-    $slide2.classList.add('centrado')
-    $slide1.classList.remove('centrado')
+    $selectRight.classList.add('r0') 
   }
   const c3fr = function(e){
     rightTocenter($slide3)
     centerToleft($slide2)
-    $slide3.classList.add('centrado')
-    $slide2.classList.remove('centrado')
+    $selectRight.classList.remove('r0')
   }
   const c4fr = function(e){
     rightTocenter($slide4)
     centerToleft($slide3)
-    $slide4.classList.add('centrado')
-    $slide3.classList.remove('centrado')
   }
   const c5fr = function(e){
     rightTocenter($slide5)
     centerToleft($slide4)
-    $slide5.classList.add('centrado')
-    $slide4.classList.remove('centrado')
+    $selectLeft.classList.add('fk')
   }
   
   $selectRight.addEventListener('click',(e)=>{
     if($slide3.classList.contains('centrado')){      
       c2fl()
-      $selectRight.classList.add('fk')
     } 
     if($slide4.classList.contains('centrado')){      
       c3fl()
@@ -115,11 +105,9 @@ window.addEventListener('DOMContentLoaded',(e)=>{
     }     
     if($slide1.classList.contains('centrado')){      
       c5fl()
-      $selectLeft.classList.add('l0')
     }    
     if($slide2.classList.contains('centrado') && $selectRight.classList.contains('r0')){
       c1fl()
-      $selectRight.classList.remove('r0')
     }
     if($selectRight.classList.contains('fk')){    
       $selectRight.classList.remove('fk')
@@ -129,23 +117,19 @@ window.addEventListener('DOMContentLoaded',(e)=>{
 
   $selectLeft.addEventListener('click',(e)=>{
     if($slide4.classList.contains('centrado')){
-      c5fr()
-      $selectLeft.classList.add('fk')
+      c5fr()      
     }
     if($slide3.classList.contains('centrado')){
       c4fr()
     }
     if($slide2.classList.contains('centrado')){      
-      c3fr()
-      $selectRight.classList.remove('r0') 
+      c3fr()       
     }
     if($slide1.classList.contains('centrado')){
-      c2fr()
-      $selectRight.classList.add('r0')   
+      c2fr()        
     }    
     if($slide5.classList.contains('centrado') && $selectLeft.classList.contains('l0')){      
-      c1fr() 
-      $selectLeft.classList.remove('l0')
+      c1fr()      
     }
     if($selectLeft.classList.contains('fk')){
       $selectLeft.classList.remove('fk')

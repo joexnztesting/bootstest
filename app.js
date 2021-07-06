5de8683a05c768368213ebeca8b02469618612fa
joexnztesting <joe.reposgithub@gmail.com>
2021-07-06 16:02:06 -0300

    5137

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const $span1 = document.querySelectorAll('.span-1');
 const $span2 = document.querySelectorAll('.span-2');
+const $span3 = document.querySelectorAll('.span-3');
+const $span4 = document.querySelectorAll('.span-4');
+const $tituProd = document.querySelector('.titu-prod');
 const $tituHuerta = document.querySelector('.titu-huerta');
 
 const $socialIcons = document.querySelectorAll('.row-icons');
@@ -18,6 +21,16 @@ entries.forEach(entry=>{
 })
 }
 const stickyOut2 = (entries)=>{
+entries.forEach(entry=>{
+  if(entry.isIntersecting){
+    if($tituProd.classList.contains('separador')){
+      $tituProd.classList.add('ocultar-titu')
+      $tituProd.classList.remove('mostrar-titu')
+    }    
+  }
+})
+}
+const stickyOut3 = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
     if($tituHuerta.classList.contains('separador')){
@@ -40,6 +53,16 @@ entries.forEach(entry=>{
 const stickyIn2 = (entries)=>{
 entries.forEach(entry=>{
   if(entry.isIntersecting){
+    if($tituProd.classList.contains('ocultar-titu')){
+      $tituProd.classList.add('mostrar-titu')
+      $tituProd.classList.remove('ocultar-titu')
+    }    
+  }
+})
+}
+const stickyIn3 = (entries)=>{
+entries.forEach(entry=>{
+  if(entry.isIntersecting){
     if($tituHuerta.classList.contains('ocultar-titu')){
       $tituHuerta.classList.add('mostrar-titu')
       $tituHuerta.classList.remove('ocultar-titu')
@@ -50,13 +73,19 @@ entries.forEach(entry=>{
 
 const observer1 = new IntersectionObserver(stickyOut, {threshold:0.4});
 const observer2 = new IntersectionObserver(stickyIn, {threshold:0.3});
-const observer3 = new IntersectionObserver(stickyOut2, {threshold:0.3});
-const observer4 = new IntersectionObserver(stickyIn2, {threshold:0.1});
+
+const observer3 = new IntersectionObserver(stickyOut2, {threshold:0.5});
+const observer4 = new IntersectionObserver(stickyIn2, {threshold:0.5});
+const observer5 = new IntersectionObserver(stickyOut3, {threshold:0.5});
+const observer6 = new IntersectionObserver(stickyIn3, {threshold:0.5});
 
 $socialIcons.forEach((el)=>observer1.observe(el));
 $marcoPlantas.forEach((el)=>observer2.observe(el));
-$span2.forEach((el)=>observer3.observe(el));
-$span1.forEach((el)=>observer4.observe(el));
+
+$span1.forEach((el)=>observer3.observe(el));
+$span2.forEach((el)=>observer4.observe(el));
+$span3.forEach((el)=>observer5.observe(el));
+$span4.forEach((el)=>observer6.observe(el));
 
 
 

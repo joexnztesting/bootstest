cd3456a8f7f0c0b3d239fc984420d2d8923e7ece
joexnztesting <joe.reposgithub@gmail.com>
2021-07-14 19:47:23 -0300

    zdkmsfh

--- app2.js
+++ app2.js
@@ -4,10 +4,22 @@ const $btn = document.querySelector('.btn-prueba');
 const acd = function(){
   alert('hi')
 }
+const ace = function(){
+  alert('hola')
+}
+const acf = function(){
+  alert('khe')
+}
+const acg = function(){
+  alert('hace')
+}
 
 if(window.PointerEvent){
   $btn.addEventListener('pointerdown', acd, true)  
 }else{
   $btn.addEventListener('touchstart', acd, true)  
+  $btn.addEventListener('touchmove', ace, true)  
+  $btn.addEventListener('touchend', acf, true)  
+  $btn.addEventListener('touchcancel', acg, true)  
 }
 

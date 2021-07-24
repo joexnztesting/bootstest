5c707bc5a54034de65fe0ec6daa258384251e4b2
joexnztesting <joe.reposgithub@gmail.com>
2021-07-23 22:01:45 -0300

    garyhadr

--- app.js
+++ app.js
@@ -1,118 +1,72 @@
-
 document.addEventListener('DOMContentLoaded',(e)=>{
-  // INICIADORES NAV
-  $panelFondo = document.querySelector('.panel-fondo');
-  $navPanel = document.querySelector('.panel5');
-  $btnHamb = document.querySelector('.btn-hamb5');
-  $navRowItem = document.querySelectorAll('.nav-row-item5 a');
-  // FIN INICIADORES NAV
 
-  // INICIADORES CAROUSEL
-  $slide = document.querySelectorAll('.slideB');   
-  let i = 0;
-  let ii = $slide.length-1  
-  $slide.forEach((slid)=>{
-    slid.style.opacity = '0'
-    slid.style.visibility = 'hidden'
-  })  
-  $slide[i].classList.add('centrado')
-  $slide[i].style.opacity = '1'
-  $slide[i].style.visibility = 'visible'  
-  if($slide[i].classList.contains('centrado')){   
-    $slide[ii].style.transform = `translateX(-${$slide.length}00%)`;
-    $slide[ii].classList.add('on-left');
-  }
-    
-  function btnLeft(){
-    if($slide[i].classList.contains('centrado')){ 
-      $slide[i].classList.remove('centrado');
-      $slide[i].style.opacity = '0'
-      $slide[i].style.visibility = 'hidden'
-      $slide[i].style.transform = `translateX(-${i+1}00%)`;
-      $slide[i].classList.add('on-left');
-      i++;      
-      if(i===$slide.length){
-        i=0
-      };
-      ii++
-      if(ii===$slide.length){
-        ii=0
-      };
-      $slide[i].style.opacity = '1';
-      $slide[i].classList.add('centrado');
-      $slide[i].style.opacity = '1'
-      $slide[i].style.visibility = 'visible'
-      $slide[i].style.transform = `translateX(-${i}00%)`;
-      if(i<$slide.length-1){
-        $slide[i+1].style.transform = `translateX(-${i}00%)`;
-      };      
-      if(i===$slide.length-2){
-        $slide[0].style.transform = `translateX(100%)`;
-      };
+  $cuadrado = document.querySelector('.caja');
+  
+  document.addEventListener('click', (e)=>{
+    if(e.target.matches('.item-1')){      
+      $cuadrado.classList.toggle('azul')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('azul')
+      },200);
+    }
+  });
+
+  document.addEventListener('touchstart', (e)=>{
+    if(e.target.matches('.item-2')){
+      $cuadrado.classList.toggle('verde')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('verde')
+      },200);
+    }
+  }, true);
+
+  document.addEventListener('touchstart', (e)=>{
+    if(e.target.matches('.item-3')){
+      $cuadrado.classList.toggle('rojo')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('rojo')
+      },200);
+    }
+  }, false);
+  
+  if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
+    var elements = document.querySelectorAll('.item');
+    var emptyFunction = function() {
+      $cuadrado.classList.toggle('naranja')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('naranja')
+      },200);
     };
-  }
-
-  function btnRight(){
-    if($slide[ii].classList.contains('on-left')){
-      
-      $slide[ii].classList.remove('on-left');
-      $slide[ii].classList.add('centrado');
-      $slide[ii].style.opacity = '1'
-      $slide[ii].style.visibility = 'visible'     
-      $slide[ii].style.transform = `translateX(-${ii}00%)`;
-      ii--      
-      
-      if(i>0){
-        $slide[i].classList.remove('centrado');
-        $slide[i].style.transform = `translateX(-${i-1}00%)`; 
-        $slide[i].style.opacity = '0'
-        $slide[i].style.visibility = 'hidden'
-      }
-      if(i===0){
-        $slide[i].classList.remove('centrado');
-        $slide[i].style.transform = `translateX(${i+1}00%)`;
-        $slide[i].style.opacity = '0'
-        $slide[i].style.visibility = 'hidden'
-        i=$slide.length
-      }
-      i-- 
-      
-      if(ii<0){
-        ii=$slide.length-1
-      }
-      $slide[ii].style.transform = `translateX(-${ii+1}00%)`;
-      $slide[ii].classList.add('on-left');       
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('touchstart', emptyFunction, false);
     }
-  }
-  // FIN INICIADORES CAROUSEL
-  
+  };
 
-  // **********************************************************
-  
-  document.addEventListener('click', e=>{
-    // NAV
-    if(e.target.matches('.btn-hamb5') || e.target.matches('.btn-hamb5 *') || e.target.matches('.item5') || e.target.matches('.panel-fondo')){
-      $panelFondo.classList.toggle('fondo-active');
-      $navPanel.classList.toggle('panel-active5');
-      $btnHamb.classList.toggle('isactive5');
-      $navRowItem.forEach((itm)=>{
-        itm.classList.toggle('item5')
-      });
-    }
-    // FIN NAV
-
-    // CAROUSEL
-    if(e.target.matches('.btn-carousel-l')){      
-      e.preventDefault();      
-      btnLeft()
-    }
-    if(e.target.matches('.btn-carousel-r')){      
-      e.preventDefault(); 
-      btnRight()
-    }
-    // FIN CAROUSEL
-  })
-
-  // **********************************************************
-
+  e.preventDefault();
 })
+
+window.onload = function() {
+  if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
+    document.body.addEventListener('touchstart', function() {
+      $cuadrado.classList.toggle('celeste')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('celeste')
+      },200);
+    }, false);
+  }
+};
+
+window.onload = function() {
+  if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
+    var elements = document.querySelectorAll('.item');
+    var emptyFunction = function() {
+      $cuadrado.classList.toggle('lima')
+      setTimeout(function(){
+        $cuadrado.classList.toggle('lima')
+      },200);
+    };
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('touchstart', emptyFunction, false);
+    }
+  }
+};

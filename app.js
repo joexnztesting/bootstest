f14bcf4105e4b415b5c71171d6590b28da3db013
joexnztesting <joe.reposgithub@gmail.com>
2021-07-25 23:15:53 -0300

    7326352

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ for(i=0; i<$slide.length; i++){
 */
 
 
-
 document.addEventListener('DOMContentLoaded',(e)=>{
-
+  
   // INICIADORES NAV
   $panelFondo = document.querySelector('.panel-fondo');
   $navPanel = document.querySelector('.panel');
   $btnHamb = document.querySelector('.btn-hamb');
   $navRowItem = document.querySelectorAll('.nav-row-item a');
+
   navShowOnOff = ()=>{
     $panelFondo.classList.toggle('fondo-active');
     $navPanel.classList.toggle('panel-active');
@@ -89,48 +89,117 @@ document.addEventListener('DOMContentLoaded',(e)=>{
     },3)
   }// FIN INICIADORES CAROUSEL
 
+// *********************************************************************
+d=document, n=navigator, ua=n.userAgent;
 
-  // ***********************************
+function userDeviceInfo(id){
+  const $id=d.getElementById(id),
+  isMobile = {
+      android:()=>ua.match(/android/i),
+          ios:()=>ua.match(/iphone|ipad|ipod/i),
+      windows:()=>ua.match(/windows phone/i),
+      any:function(){
+          return this.android()||this.ios()||this.windows();
+      }
+  },
+  isDesktop = {
+      linux:()=>ua.match(/linux/i),
+      mac:()=>ua.match(/mac os/i),
+      windows:()=>ua.match(/windows nt/i),
+      any:function(){
+        return this.linux()||this.mac()||this.windows();
+      }
+  },
+  isBrowser = {
+      chrome:()=>ua.match(/chrome/i),
+      safarai:()=>ua.match(/safarai/i),
+      firefox:()=>ua.match(/firefox/i),
+      opera:()=>ua.match(/opera|opera mini/i),
+      ie:()=>ua.match(/msie|iemobile/i),
+      edge:()=>ua.match(/edge/i),
+      any:function(){
+        return(
+          this.ie()||
+          this.edge()||
+          this.chrome()||
+          this.safarai()||
+          this.firefox()||
+          this.opera()
+        );
+      }
+  };
 
-  // LLAMADORES
+  $id.innerHTML=`
+  <ul>
+      <li>User agent: <b>${ua}</b></li>
+      <li>Plataforma: <b>${isMobile.any()? isMobile.any():isDesktop.any()}</b></li>
+      <li>Navegador: <b>${isBrowser.any()}</b></li>
+  </ul>
+  `
 
-  document.addEventListener('click', (e)=>{ 
-    // nav
-    if(e.target.matches('.btn-hamb') || e.target.matches('.btn-hamb *') || e.target.matches('.item') || e.target.matches('.panel-fondo')){
-      navShowOnOff()
-    }// fin nav    
-    
-    // carousel
-    if(e.target.matches('.btn-carousel-l')){
-      toLeft()
+  if(isBrowser.chrome()){
+    $id.innerHTML+=`<p>Este contenido es exclusivo Chrome</p>`
+  }
+  if(isBrowser.firefox()){
+      $id.innerHTML+=`<p>Este contenido es exclusivo Firefox</p>`
     }
-    if(e.target.matches('.btn-carousel-r')){
-      toRight()
-    }// fin carousel
-  });
+  if(isDesktop.linux()){
+      $id.innerHTML+=`<p>Descarga nuestro Software para Linux</p>`
+    }
+  if(isDesktop.windows()){
+      $id.innerHTML+=`<p>Descarga nuestro Software para Windows</p>`
+    }
 
+  if(isMobile.android() || isBrowser.any()){
+    document.addEventListener('click', (e)=>{ 
+      // nav
+      if(e.target.matches('.btn-hamb') || e.target.matches('.btn-hamb *') || e.target.matches('.item') || e.target.matches('.panel-fondo')){
+        navShowOnOff()
+      }// fin nav    
+      
+      // carousel
+      if(e.target.matches('.btn-carousel-l')){
+        toLeft()
+      }
+      if(e.target.matches('.btn-carousel-r')){
+        toRight()
+      }// fin carousel
+    });
+  }
 
-  document.addEventListener('touchstart', (e)=>{
-    // nav
-    //if(e.target.matches('.btn-hamb') || e.target.matches('.btn-hamb *') || e.target.matches//('.item') || e.target.matches('.panel-fondo')){
-    //  navShowOnOff()
-    //}// fin nav
+  if(isMobile.ios()){
+    document.addEventListener('touchstart', (e)=>{
+      // nav
+      if(e.target.matches('.btn-hamb') || e.target.matches('.btn-hamb *') || e.target.matches('.item') || e.target.matches('.panel-fondo')){
+        navShowOnOff()
+      }// fin nav
+  
+      // carousel
+      if(e.target.matches('.btn-carousel-l')){
+        toLeft()
+      }
+      if(e.target.matches('.btn-carousel-r')){
+        toRight()
+      }// fin carousel
+  
+    }, true);
+  }       
+  //redireccionando
+  //if(isMobile.android()){
+  //    let option = confirm(`Estas desde Android, quieres arir el redireccionamiento?`)
+  //    if(option)window.location.href='https://jonmircha.com'
+  //}
+}  
+userDeviceInfo('user-device')
 
-    // carousel
-    //if(e.target.matches('.btn-carousel-l')){
-    //  toLeft()
-    //}
-    //if(e.target.matches('.btn-carousel-r')){
-    //  toRight()
-    //}// fin carousel
-
-  }, true);
-
+// *********************************************************************
 
   e.preventDefault();
 })
 
-// ***************************************
+
+
+
 
 
 
